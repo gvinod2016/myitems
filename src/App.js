2516bb400961fec8930b220d77d1e1339f777883
Vinod <vinodkumar.tpt2016@gmail.com>
2023-12-14 11:32:55 +0530

    import provider

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import './App.css';
 import Home from "./fecters/home";
 import About from "./fecters/about";
 import Layout from "./layout";
-
-
+import store from './store'
+import { Provider } from 'react-redux'
+// console.log(store, 'store....')
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="home" element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Route>
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Route>
+        </Routes>
+      </Provider>
     </BrowserRouter>
   );
 }
