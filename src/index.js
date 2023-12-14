0dc4aeda41bd932dfc7b31bef178a1e1a9d235a2
Vinod <vinodkumar.tpt2016@gmail.com>
2023-12-14 11:45:16 +0530

    store add

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// console.log(store.getState(), 'store...')
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
 
