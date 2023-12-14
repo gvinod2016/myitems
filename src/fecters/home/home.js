de581a614a09614d4177a05d45541b3c5dcf125d
Vinod <vinodkumar.tpt2016@gmail.com>
2023-12-14 11:49:20 +0530

    add proprs data in home

--- src/fecters/home/home.js
+++ src/fecters/home/home.js
@@ -2,33 +2,96 @@
 import { useEffect, useState } from "react"
 import axios from 'axios';
 
-// json config data
+// json config data 
 // import { _items } from "../../../mock/items"
 
 // css
 import "./home.css"
 import ProductList from "./product-list/product-list";
+import SearchBare from "../../component/search-bare";
+import Tittle from "../../component/tittle";
+import Filterdata from "../../component/filter-data";
+import { useDispatch, useSelector } from 'react-redux'
 
-export default function Home() {
+export default function Home(props) {
+    const dispatch = useDispatch()
+    // const selector = useSelector()
+
+    // const countVal = useSelector(state => {
+    //     console.log(state, 'state')
+    //     return state
+    // })
+    const { count, addCount } = props
+    console.log(count, 'countVal...')
     const [productList, setProductList] = useState(null)
+    const [searchData, setSearchData] = useState(null)
     useEffect(() => {
         const url = './mock/items.json'
         axios.get(url).then((res) => {
             const data = res.data;
-            console.log(data, "mock-data")
+            // console.log(data, "mock-data")
             setProductList(data)
+            // console.olog(data)
         }).catch((err) => {
             console.log(err, 'error-data')
             setProductList(err)
         })
     }, [])
+
+    // const onchaigeData = (e) => {
+    //     const inputVal = e && e?.target?.value
+    //     // const inputName = e && e?.target?.name
+    //     // setSearchData({...searchData, [inputName]:inputVal})
+    //     setSearchData(inputVal, "search....")
+    //     console.log(inputVal, "inputvalue", typeof (searchData), "search....")
+
+    // }
+    // const onclickData = () => {
+    //     console.log(productList, "data....")
+    //     const filterData = productList.filter((row) => {
+    //         if (row.name.toLowerCase().includes(searchData.toLowerCase())) {
+    //             console.log(row, "row")
+    //             return row
+    //         }
+    //     })
+    //     console.log(filterData, "fff...")
+    //     setProductList(filterData)
+    // }
+    // const recentList = productList.filter((row)=>{
+    //             if(row.type=="recent"){
+
+    //             }
+    // })
+    const addCountVal = () => {
+        dispatch(addCount(1))
+    }
     if (!productList) {
-        return 'loading....'
+        return <div className="text-center">
+            <h2>loading....</h2>
+        </div>
     }
     return (
         <>
-            <div className="sec-home">
-                <ProductList productList={productList}></ProductList>
+            <div className="sec-home row m-0 px-5 pb-4">
+                <nav className="nave-bar col-2 ">
+                    <h1 onClick={addCountVal}>Ttitle</h1>
+                    {/* <Filterdata></Filterdata>* */}
+                </nav>
+                <article className="col-10">
+                    {/* <SearchBare onchaigeData={onchaigeData} onclickData={onclickData}></SearchBare> */}
+                    {
+                        productList && productList.map((row) => {
+                            return (
+                                <>
+                                    <div>
+                                        <Tittle row={row}></Tittle>
+                                        <ProductList row={row.prodect.slice(0, 6)}></ProductList>
+                                    </div>
+                                </>
+                            )
+                        })
+                    }
+                </article>
             </div>
         </>
     )
--- src/fecters/home/index.js
+++ src/fecters/home/index.js
@@ -1,3 +1,22 @@
+import { connect } from "react-redux";
+import { addCount } from "../../store/countReducer";
 import Home from "./home";
 
-export default Home
+const mapStateToProps = ({home}) => {
+    // const { home } = state
+    const { count } = home;
+    console.log( home, 'home...', count)
+
+    return {
+        count 
+    }
+}
+
+const mapDisptchToProps = {
+    addCount
+}
+
+
+const connectHomePage = connect(mapStateToProps, mapDisptchToProps)(Home)
+
+export default connectHomePage 
